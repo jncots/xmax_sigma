7227caebceb87a5d51ed6522edb5a76b970c486f
Anton Prosekin <ayprosekin@gmail.com>
2023-02-02 16:12:05 +0800

    remove debug code

--- cascade/pythia_decay.py
+++ cascade/pythia_decay.py
@@ -48,7 +48,6 @@ class Pythia8DecayAfterburner:
             )
             self.pythia.particleData.mayDecay(particle.pid, True)
 
-        # print(f"Event.size = {len(event)}")
         # Decay it
         self.pythia.forceHadronLevel()
         ievent = 0
@@ -101,45 +100,6 @@ class Pythia8DecayAfterburner:
                     )
                     final_particles.append(particle)
             ievent += 1        
-                    
-            # if p.mother1() == 0:
-            #     if p.isFinal():
-            #         final_particles.append(all_particles[ievent])
-            # else:
-            #     if p.isFinal():
-            #         mother_index = p.mother1() - 1
-            #         if p.mother1() > len(event):
-            #             mother_info[p.mother1()]
-            #             mother_info.append((p.id(), p.e(), p.mother1(), p.isFinal()))
-            #             mother = CascadeParticle(
-            #         pid=mother_info[p.mother1()][0],
-            #         energy=mother_info[p.mother1()][1],
-            #         xdepth=mother.xdepth_decay,
-            #         production_mode=2,
-            #         generation_number=mother.generation_number + 1,
-            #         parent=[mother],
-            #     )
-                        
-                        
-                    
-            #     # print(f"Event.size = {len(event)}")
-            #     # print(f"p_mother =  {p.mother1()} ")
-            #     try:
-            #         mother = event[p.mother1() - 1]
-            #     except:
-            #         print(f"Event.size = {len(event)}")
-            #         print(f"p_mother =  {p.mother1()} ")
-
-            #     cp = CascadeParticle(
-            #         pid=p.id(),
-            #         energy=p.e(),
-            #         xdepth=mother.xdepth_decay,
-            #         production_mode=2,
-            #         generation_number=mother.generation_number + 1,
-            #         parent=[mother],
-            #     )
-            #     new_event.append(cp)
-            # ievent += 1
         
         # for particle in all_particles:    
         #     print(particle)    
@@ -150,77 +110,7 @@ class Pythia8DecayAfterburner:
             
         return final_particles
 
-    def _debug_call(self, event):
-
-        new_event = []
-
-        self.pythia.event.reset()
-
-        for particle in event:
-            m0 = self.pythia.particleData.findParticle(particle.pid).m0
-            self.pythia.event.append(
-                particle.pid,
-                91,
-                0,
-                0,
-                0,
-                0,
-                np.sqrt((particle.energy + m0) * (particle.energy - m0)),  # pz
-                particle.energy,
-                m0,
-            )
-            self.pythia.particleData.mayDecay(particle.pid, True)
-
-        # print(f"Event.size = {len(event)}")
-        # # Decay it
-        self.pythia.forceHadronLevel()
-        ievent = 0
-
-        #         .def("id", [](const Particle& particle){ return particle.id();})
-        # .def("status", [](const Particle& particle){ return particle.status();})
-        # .def("tau", [](const Particle& particle){ return particle.tau();})
-        # .def("px", [](const Particle& particle){ return particle.px();})
-        # .def("py", [](const Particle& particle){ return particle.py();})
-        # .def("pz", [](const Particle& particle){ return particle.pz();})
-        # .def("e", [](const Particle& particle){ return particle.e();})
-        # .def("m", [](const Particle& particle){ return particle.m();})
-        # .def("mother1", [](const Particle& particle){ return particle.mother1();})
-        # .def("mother2", [](const Particle& particle){ return particle.mother2();})
-        # .def("isFinal", [](const Particle& particle){ return particle.isFinal();})
-        ievent = 0
-        for p in self.pythia.event:
-            print(
-                f"i = {ievent}, id = {p.id()}, status = {p.status()},",
-                f" energy = {p.e()}, mother1 = {p.mother1()}",
-                f" final = {p.isFinal()}, mother2 = {p.mother2()}"
-            )
-            ievent += 1
-        #     # Skip first record (it is not relevant)
-        #     if ievent == 0:
-        #         ievent += 1
-        #         continue
-
-        #     if p.mother1() == 0:
-        #         if p.isFinal():
-        #             new_event.append(event[ievent - 1])
-        #     else:
-        #         # print(f"Event.size = {len(event)}")
-        #         # print(f"p_mother =  {p.mother1()} ")
-        #         try:
-        #             mother = event[p.mother1() - 1]
-        #         except:
-        #             print(f"Event.size = {len(event)}")
-        #             print(f"p_mother =  {p.mother1()} ")
-
-        #         cp = CascadeParticle(pid = p.id(),
-        #                             energy = p.e(),
-        #                             xdepth = mother.xdepth_decay,
-        #                             production_mode = 2,
-        #                             generation_number = mother.generation_number + 1,
-        #                             parent = [mother])
-        #         new_event.append(cp)
-        #     ievent += 1
-        # return new_event
+    
 
 
 if __name__ == "__main__":
@@ -229,8 +119,6 @@ if __name__ == "__main__":
         CascadeParticle(2212, 1e3, 77),
         CascadeParticle(111, 13e2, 888)
     ]
-
-    # afterburner._debug_call(event111)
 
     event = afterburner(event111)
 
